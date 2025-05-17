1b25858e44b25960fbe518466f2ddae529d3e152
karimpinkman <gamerkarim1234@gmail.com>
2025-05-17 22:23:39 +0100

    I modified the index format and i added some changes to the index

--- index.php
+++ index.php
@@ -33,11 +33,41 @@
   </div>
 </nav>
 <h1>LOUD STREETZ 2025</h1>
-<div>
-    <img src="" alt=""><img src="" alt=""><img src="" alt="">
-    <hr color="" width="30%" >
+<br>
+<div class="container" >
+    <img class="IMG1" src="images/index_pictures/carti.png" alt=""><img class="IMG1" src="images/index_pictures/travis.png" alt=""><img class="IMG1" src="images/index_pictures/ye.png" alt="">
 </div>
-<h1>SIGN UP TO THE FESTIVAL </h1><br>
-<div>BE THE FIRST TO ACCES AND GET</div>
+    <br><hr color="#cfcfcf80;" width="2px" >
+
+<section class="form-section text-center">
+    <h1>SIGN UP TO THE FESTIVAL </h1>
+    <p class="mb-4">BE THE FIRST TO ACCESS FOR THE UPCOMING FESTIVALS, SIGN UP AND DON'T MISS THE LAST SHOWS!</p>
+        <form method="post" action="" >
+      <div class="form-group">
+        <label for="firstName">First Name</label>
+        <input name="firstName" type="text" id="firstName" placeholder="Enter a first name...">
+      </div>
+
+      <div class="form-group">
+        <label for="lastName">Last Name</label>
+        <input name="lastName" type="text" id="lastName" placeholder="Enter a last name...">
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input name="Email" type="email" id="email" placeholder="Enter an email...">
+      </div>
+
+      <div class="form-group">
+        <label for="phone">Phone Number</label>
+        <input name="phone" type="tel" id="phone" placeholder="Enter a phone number...">
+      </div>
+
+
+      <div class="form-group" style="flex: 1 1 100%; text-align: center;">
+        <button type="submit" class="submit-button">SIGN UP</button>
+      </div>
+    </form>
+</section>
 </body>
 </html>
