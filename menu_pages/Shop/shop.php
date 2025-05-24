8d4ba4339502304e9341d32868f17f7cf1599007
Taha Darif <dariif023@gmail.com>
2025-05-24 13:32:48 +0100

    I fixed the view in the SHOP PAGE.

--- menu_pages/Shop/shop.php
+++ menu_pages/Shop/shop.php
@@ -42,12 +42,12 @@
       while ($ligne = $sql->fetch()) {?>
         <div class="prdct">
           <img src="<?php echo $ligne['thumb']; ?>" alt="" style="width: 100%; height:fit-content;">
-          <div style="color: #cfcfcf; font-size:x-large"><?php echo $ligne['name']; ?></div>
-          <div style="height: 70px;"><?php echo $ligne['description']; ?></div>
+          <div style="color: #cfcfcf; font-size:x-large;height: 70px;"><?php echo $ligne['name']; ?></div>
+          <div style=""><?php echo $ligne['description']; ?></div>
           <div style="color: #cfcfcf;font-size:x-large">Price: <?php echo $ligne['prix']; ?>$</div>
           <br>
           <div style="justify-content: flex-start;justify-self:flex-end">
-            <div class="btn-price-container">
+            <div class="">
             <a style="width:100%" href="buyPage/buyPage.php?id=<?= $ligne["produitId"] ?>"><button type="submit" class="submit-button" style="padding: 10px;width:100%;">BUY</button></a>
             </div>
           </div>
